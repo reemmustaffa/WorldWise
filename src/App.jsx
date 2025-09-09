8a157766e01ca4c6d78ed43ea7b3cb52fe0f3605
reem_mustafa <reemmustafa905@gmail.com>
2025-09-09 19:26:02 +0300

    Implementing main pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 // import { useEffect, useState } from "react";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+import Homepage from "./Pages/Homepage";
+import Pricing from "./Pages/Pricing";
+import PageNotFound from "./Pages/PageNotFound";
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
