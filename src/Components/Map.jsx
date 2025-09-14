e7181c71fafbbc21f81138886080bcb5babf05b6
reem_mustafa <reemmustafa905@gmail.com>
2025-09-14 10:42:21 +0300

    inetracting with the map

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,23 +1,40 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "../Contexts/CitiesContext";
 
 function Map() {
-  const navigate = useNavigate();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat"); // lat دي الللي انا بعتتاها في الليك ك query string
-  const lng = searchParams.get("lng"); // lng دي بردو اللي انا بعتها في اللينك
+  // قراءة query string من الـ URL
+
+  const [searchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat"); // lat دي الللي انا بعتتاها في الليك ك query string
+  const mapLng = searchParams.get("lng"); // lng دي بردو اللي انا بعتها في اللينك
+
+  // تحديث مكان الماب كلما الـ URL يتغير
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     <div className={styles.mapContainer}>
       {/* INCLUDING MAP WITH LEAFLET LIBRARY */}
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={8}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -25,14 +42,45 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
 }
 
+// مسؤول عن تغيير center بتاع الماب
+
+function ChangeCenter({ position }) {
+  //Hook useMap بيديك access على الماب نفسها.
+  const map = useMap();
+
+  //بيخلي الماب تتحرك للـ position الجديد لو الـ state اتغير.
+  map.setView(position);
+  return null;
+}
+
+// مسؤول عن التقاط click على الماب والتنقل لصفحة form مع الإحداثيات
+function DetectClick() {
+  const navigate = useNavigate();
+
+  //Hook useMapEvent بيسمع لأي click على الماب.
+  useMapEvent({
+    click: (e) => {
+      console.log(e);
+      //لما تدوس على الماب → بياخد الإحداثيات → يعمل navigate لصفحة form ويضيف الـ lat/lng في الـ URL.
+      navigate(`form?lat=${e.latlng.lat}&lang=${e.latlng.lng}`);
+    },
+  });
+}
 export default Map;
